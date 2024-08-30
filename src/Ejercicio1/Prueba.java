45c99a6cc42486af6de780c736bbb4ca8ca0398b
idiomas <idiomas@PC5-SJ105.fup.edu>
2024-08-29 21:28:51 -0500

    HELLO

---
+++ src/Ejercicio1/Prueba.java
@@ -0,0 +1,13 @@
+
+package Ejercicio1;
+
+
+public class Prueba {
+
+   
+    public static void main(String[] args) {
+       Datos objeto = new Datos();
+       objeto.mostrarMenu();
+    }
+    
+}
